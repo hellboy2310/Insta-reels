f272d3af732a1b13eda8da91296f7401a8c9577d
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-02-13 06:53:56 +0530

    login-sample

--- instagram-app/src/components/Login.jsx
+++ instagram-app/src/components/Login.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+
 function Login(){
+    const[email,setEmail] = useState('');
+    const[pass,setpass]  = useState('');
+    
+    const trackEmail = (e) =>{
+        setEmail(e.target.value);
+    }
+    const trackPass = (e) =>{
+        setpass(e.target.value);
+    }
+
+    const handlebutton = () =>{
+        alert(email  + " " + pass);
+    }
+
+
     return(
-        <div>Login</div>
+        <div>
+            <input placeholder="enter your email" onChange={trackEmail} type = 'email'></input>
+            <br></br>
+            <input placeholder="enter your password" onChange = {trackPass} type = 'password'></input>
+            <br></br>
+            <button onClick={handlebutton}>Login</button>
+
+        </div>
     )
 }
 export default Login;
