973adc4cf274a3a80d8fd5a6152dd74c613f7144
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-02-19 11:29:29 +0530

    comment box

--- instagram-app/src/components/VideoContainer.jsx
+++ instagram-app/src/components/VideoContainer.jsx
@@ -1,30 +1,64 @@
+
+import './videocontainer.css'
 import { useState } from "react"
 
 
 
-function VideoContainer(){
-    let[playing,setPlaying] = useState(false); 
-    
-    return(
-        <>
-        <div className="video_card">
-            <p>Fake Name</p>
-            <span>Song Name</span>
+function VideoContainer() {
+    let [playing, setPlaying] = useState(false);
+    let [commentbox, setComment] = useState(false);
+    return (
 
-            <video onClick={(e)=>{
-                if(playing){
-                e.currentTarget.pause();
-                setPlaying(false);
-                }
-                else{
-                    e.currentTarget.play();
-                    setPlaying(true);
-                }
-            }} 
-             src = ""></video>
+            <div className="video_card">
+                <p className='video-card-username' >Fake Name</p>
+                <span className='video-card-music'>Song Name</span>
 
-        </div>
-        </>
+                <span  onClick={(e)=>{
+                    if(setComment){
+                        setComment(true);
+                        console.log("comment was pressed");
+                    }
+                    else{
+                        setComment(false);
+                    }
+                }}>
+                   <button class = "chat_button">comment</button> 
+                </span>
+
+              {commentbox ? 
+              <div className="video-card-comment-box">
+                <div className="actual-comments">
+                    <h5>User Name</h5>
+                    <p>This is actual comment</p>
+                </div>
+                <div className="comment-form">
+                    <div className="post-user-comment">
+
+                        <input type = 'text'/>
+                        <button>post</button>
+                    </div>
+
+                </div>
+
+              </div>:("")}
+                
+
+
+
+                <video onClick={(e) => {
+                    if (playing) {
+                        e.currentTarget.pause();
+                        setPlaying(false);
+                    }
+                    else {
+                        e.currentTarget.play();
+                        setPlaying(true);
+                    }
+                }}
+                    src=""></video>
+
+            </div>
+
     )
 }
 
