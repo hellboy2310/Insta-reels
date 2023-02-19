afb86b653d42c24986d31838153e13ee2a86698c
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-02-19 13:42:13 +0530

    firebase file uploaded

--- instagram-app/src/firebase.js
+++ instagram-app/src/firebase.js
@@ -5,6 +5,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import secret from './secrets';
+import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 
 
@@ -12,3 +13,4 @@ import secret from './secrets';
 const app = initializeApp(secret);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
